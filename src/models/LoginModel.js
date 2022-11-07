935084b838bcf8e9cb54d534ee1fef799d6b46fc
Pedro Moura <henriquepedro3223@protonmail.com>
2022-11-07 16:56:01 -0300

    added function to verify if user exist. And another function to protect the password in DB

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
     email:  { type: String, required: true },
@@ -19,11 +20,22 @@ class Login {
         this.valida()
         if(this.errors.length > 0) return
 
+        await this.userExist() //Validando se usuário já existe
+        if(this.errors.length > 0) return
+
         try {
-            this.user = await LoginModel.create(this.body)
+            const salt = bcryptjs.genSaltSync() // o que é isso
+            this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+            this.user = await LoginModel.create(this.body) // CRIAÇÃO DO USUÁRIO
         } catch (e) {
             console.log(e)
         }
+    }
+    // VALIDAÇÃO SE USUÁRIO JÁ EXISTE
+    async userExist() {
+        const user = await LoginModel.findOne({ email: this.body.email })
+        if(user) this.errors.push('Usuário já existe.')
     }
 
     valida() {
